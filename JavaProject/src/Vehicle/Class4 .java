ac91a23d8fdaa2be563135aeadc800b0eb062f0b
neeraj043 <38494829+neeraj043@users.noreply.github.com>
2026-01-16 12:57:05 +0530

    Login Page Changes
    Login Page Changes

---
+++ JavaProject/src/Vehicle/Class4 .java
@@ -0,0 +1,14 @@
+package javaPackage;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Class1 cl=new Class1();
+		cl.car_model=2009;
+		
+
+	}
+
+}
