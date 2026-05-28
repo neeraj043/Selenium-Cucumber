e18b1b84f5e40011109e09aa73ee3ae2c52af1ab
neeraj043 <38494829+neeraj043@users.noreply.github.com>
2026-05-28 16:14:18 +0530

    28 May Changes
    28 May Changes

--- Class3 - Copy (2).java
+++ Class3 - Copy (2).java
@@ -7,7 +7,18 @@ public class Class2 {
 		
 		Class1 cl=new Class1();
 		cl.car_model=2009;
-		
+		Class1 cl=new Class1();
+		cl.car_model=2009;
+Class1 cl=new Class1();
+		cl.car_model=2009;
+Class1 cl=new Class1();
+		cl.car_model=2009;
+Class1 cl=new Class1();
+		cl.car_model=2009;
+Class1 cl=new Class1();
+		cl.car_model=2009;
+Class1 cl=new Class1();
+		cl.car_model=2009;
 
 	}
 
